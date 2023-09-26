fc3c12151a1191d9830de829aecaf51004722ed0
aldilukmn <aldilukman99@gmail.com>
2023-09-26 21:17:01 +0700

    parallax rev 2

--- assets/js/index.js
+++ assets/js/index.js
@@ -45,15 +45,15 @@ window.addEventListener('scroll', function() {
   const grid_card_why_us = document.querySelectorAll('#why-us .row .col');
   
   if (1200 > window.innerWidth ) {
-    if (ver_scroll > why_us_title_offsetTop - 500) {
+    if (ver_scroll > why_us_title_offsetTop - 700) {
       why_us_title.classList.add('op-100');
     }
     
     grid_card_why_us.forEach(function(col, i) {
-      if (ver_scroll > why_us_title_offsetTop - 350) {
+      if (ver_scroll > why_us_title_offsetTop - 500) {
         setTimeout(function() {
           grid_card_why_us[i].classList.add('op-100');
-        }, 350 * (i + 1))
+        }, 100 * (i + 1))
       }
     })
   } else {
@@ -65,11 +65,79 @@ window.addEventListener('scroll', function() {
       if (ver_scroll > why_us_title_offsetTop - 300) {
         setTimeout(function() {
           grid_card_why_us[i].classList.add('op-100');
-        }, 350 * (i + 1))
+        }, 100 * (i + 1))
       }
     })
   }
 
+  // Testimonial section
+  const testimonial_title = document.getElementsByClassName('testimonial-title')[0];
+  const testimonial_title_offsetTop = testimonial_title.offsetTop;
+  const carousel_card = document.getElementsByClassName('carousel')[0];
+  const carousel_card_offsetTop = carousel_card.offsetTop;
 
+  if (1200 > window.innerWidth) {
+    if (ver_scroll > testimonial_title_offsetTop - 900) {
+      testimonial_title.classList.add('op-100');
+    }
+  
+    if (ver_scroll > carousel_card_offsetTop - 700) {
+      carousel_card.classList.add('op-100');
+    }
+  } else {
+    if (ver_scroll > testimonial_title_offsetTop - 500) {
+      testimonial_title.classList.add('op-100');
+    }
+  
+    if (ver_scroll > carousel_card_offsetTop - 300) {
+      carousel_card.classList.add('op-100');
+    }
+  }
+
+  // Another section
+  const another_section = document.getElementsByClassName('another-section')[0];
+  const another_section_offsetTop = another_section.offsetTop;
+
+ if (1200 > window.innerWidth) {
+  if (ver_scroll > another_section_offsetTop - 900) {
+    another_section.classList.add('op-100');
+  } else {
+    if (ver_scroll > another_section_offsetTop - 450) {
+      another_section.classList.add('op-100');
+  }
+ }
+}
+
+  // FAQ section
+  const faq_left = document.getElementsByClassName('faq-left')[0];
+  const faq_right = document.getElementsByClassName('faq-right')[0];
+  const faq_section = document.getElementById('faq');
+  const faq_section_offsetTop = faq_section.offsetTop;
+
+  if (1200 > window.innerWidth) {
+    if (ver_scroll > faq_section_offsetTop - 900) {
+      faq_left.classList.add('op-100');
+      faq_right.classList.add('op-100');
+    }
+  } else {
+    if (ver_scroll > faq_section_offsetTop - 400) {
+      faq_left.classList.add('op-100');
+      faq_right.classList.add('op-100');
+    }
+  }
+
+  // Footer section
+  const footer_section = document.getElementsByTagName('footer')[0];
+  const footer_section_offsetTop = footer_section.offsetTop;
+
+  if (1200 > window.innerWidth) {
+    if (ver_scroll > footer_section_offsetTop - 1000) {
+      footer_section.classList.add('op-100');
+    }
+  } else {
+    if (ver_scroll > footer_section_offsetTop - 650) {
+      footer_section.classList.add('op-100');
+    }
+  }
 
 })
