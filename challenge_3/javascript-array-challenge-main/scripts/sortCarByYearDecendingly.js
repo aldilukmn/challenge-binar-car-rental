f34aeb05deb5e566789e9a26adbb4a5acbffda01
aldilukmn <aldilukman99@gmail.com>
2023-10-12 19:41:04 +0700

    result of challenge_3

---
+++ challenge_3/javascript-array-challenge-main/scripts/sortCarByYearDecendingly.js
@@ -0,0 +1,25 @@
+function sortCarByYearDescendingly(cars) {
+  // Sangat dianjurkan untuk console.log semua hal hehe
+  console.log(cars);
+
+  // Clone array untuk menghindari side-effect
+  // Apa itu side effect?
+  const result = [...cars];
+
+  // Tulis code-mu disini
+
+  const lengthArr = result.length;
+
+  for(let i = 0; i < lengthArr - 1; i++) {
+    for(let j = 0; j < lengthArr - 1; j++) {
+      if(result[j].year < result[j + 1].year) {
+        let temp = result[j];
+        result[j] = result[j + 1];
+        result[j + 1] = temp;
+      }
+    }
+  }
+
+  // Rubah code ini dengan array hasil sorting secara descending
+  return result;
+}
